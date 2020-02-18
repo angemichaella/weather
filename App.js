c0fd35863c15620a72e6cf49bdd2cb53f94c5d86
Ange Michaella <angemichaella@yahoo.fr>
2020-02-17 22:34:08 -0500

    completed basic features
    API (MetaWeather) works well with app

--- App.js
+++ App.js
@@ -1,28 +1,105 @@
 import React from 'react';
-import { StyleSheet, Text, View, KeyboardAvoidingView, ImageBackground } from 'react-native';
-import { Platform } from '@unimodules/core';
+import { StyleSheet,
+   Text, 
+   View, 
+   KeyboardAvoidingView, 
+   ImageBackground,
+   Platform,
+   ActivityIndicator,
+   StatusBar,
+  } from 'react-native';
 
+import {fetchLocationId, fetchWeather} from './utils/api.js';
 import getImageForWeather from './utils/getImageForWeather';
 
 import SearchInput from './components/SearchInput';
 
 export default class App extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: '',
+      loading: false,
+      error: false,
+      temperature: 0,
+      weather: '',
+    };
+  }
+
+  componentDidMount() {
+    this.handleUpdateLocation('Nairobi');
+  }
+
+  handleUpdateLocation = async city => {
+    if(!city) return;
+
+    this.setState({loading: true}, async () => {
+      try {
+          const locationId = await fetchLocationId(city);
+          const {location, weather, temperature} = await fetchWeather(locationId);
+
+          this.setState({
+            loading:false,
+            error: false,
+            location,
+            weather, 
+            temperature,
+          });
+      } catch(e) {
+        this.setState({
+          loading: false,
+          error: true,
+        });
+      }
+    });
+  }
+
   render () {
+
+    const {
+      loading,
+      error,
+      location,
+      weather,
+      temperature,
+    } = this.state; 
+
     return (
       <KeyboardAvoidingView 
         style={styles.container}
         behavior = 'padding'
       >
+        <StatusBar barStyle='light-content'/>
         <ImageBackground
-          source = {getImageForWeather('Clear')}
+          source = {getImageForWeather(weather)}
           style = {styles.imageContainer}
           imageStyle = {styles.image}
         >
           <View style = {styles.detailsContainer}>
-            <Text style = {[styles.largeText, styles.textStyle]}>Gitega</Text>
-            <Text style = {[styles.smallText, styles.textStyle]}>Light Cloud</Text>
-            <Text style = {[styles.largeText, styles.textStyle]}>24°</Text>
-            <SearchInput placeholder = "Search any city"/>
+            <ActivityIndicator
+              animating= {loading}
+              color= 'white'
+              size = 'large'
+            />
+
+            {!loading && (
+              <View>
+                {error && (
+                  <Text style = {[styles.smallText, styles.textStyle]}>
+                    Could not load weather, please try a different city
+                  </Text>
+                )}
+                {!error && (
+                  <View>
+                    <Text style = {[styles.largeText, styles.textStyle]}>{location}</Text>
+                    <Text style = {[styles.smallText, styles.textStyle]}>{weather}</Text>
+                    <Text style = {[styles.largeText, styles.textStyle]}>{`${Math.round(temperature)}°`}</Text>
+                  </View>
+                )}
+                <SearchInput placeholder = "Search any city" onSubmit = {this.handleUpdateLocation}/>
+              </View>
+            )}
           </View>
         </ImageBackground>
       </KeyboardAvoidingView>
@@ -43,6 +120,12 @@ const styles = StyleSheet.create({
     width: null,
     height: null,
     resizeMode: 'cover',
+  }, 
+  detailsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20,
   },
   largeText: {
     fontSize: 44,
@@ -56,10 +139,4 @@ const styles = StyleSheet.create({
       Platform.OS == 'ios' ? 'AvenirNext-Regular' : 'Roboto',
     color: 'white',
   }, 
-  detailsContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0,0,0,2)',
-    paddingHorizontal: 20,
-  },
 });
--- utils/getImageForWeather.js
+++ utils/getImageForWeather.js
@@ -6,6 +6,7 @@ const images = {
   'Heavy Cloud': require('../assets/heavy-cloud.png'),
   'Light Cloud': require('../assets/light-cloud.png'),
   'Heavy Rain': require('../assets/heavy-rain.png'),
+  'Light Rain': require('../assets/light-rain.png'),
   Showers: require('../assets/showers.png'),
   Sleet: require('../assets/sleet.png'),
   Snow: require('../assets/snow.png'),
